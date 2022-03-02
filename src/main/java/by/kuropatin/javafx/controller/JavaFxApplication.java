54c7ddf8dba27eceef8ea4f9ef1b5945b116854c
Pavel Puropatin <kuropatin.pv@gmail.com>
2022-03-02 20:31:52 +0300

    Works!

--- src/main/java/by/kuropatin/javafx/controller/JavaFxApplication.java
+++ src/main/java/by/kuropatin/javafx/controller/JavaFxApplication.java
@@ -4,28 +4,34 @@ import by.kuropatin.javafx.SpringBootStarter;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.io.IOException;
 
 public class JavaFxApplication extends Application {
 
     private ConfigurableApplicationContext context;
+    private Parent root;
 
     @Override
-    public void init() {
-        context = new SpringApplicationBuilder(SpringBootStarter.class).run();
+    public void init() throws IOException {
+        context = SpringApplication.run(SpringBootStarter.class);
+        final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-view.fxml"));
+        fxmlLoader.setControllerFactory(context::getBean);
+        root = fxmlLoader.load();
     }
 
     @Override
-    public void start(final Stage stage) throws Exception {
+    public void start(final Stage stage) {
         context.publishEvent(new StageReadyEvent(stage));
 
-        final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-view.fxml"));
-        final Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        final Scene scene = new Scene(root, 600, 400);
         final Image icon = new Image("icon.png");
 
         stage.getIcons().add(icon);
